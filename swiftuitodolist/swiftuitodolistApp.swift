f2d2fcb45b7094558344da6df37398ba3ce74308
Süleyman Çambel <cambelsuleyman@gmail.com>
2023-12-04 01:24:02 +0300

    First Upload

---
+++ swiftuitodolist/swiftuitodolistApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftuitodolistApp.swift
+//  swiftuitodolist
+//
+//  Created by Süleyman Çambel on 4.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct swiftuitodolistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
